a34ca1363647cbfcd82967361f373f1f9be6a200
Landry Dupont <landrydupont790@gmail.com>
2025-02-25 09:31:09 +0100

    delete photo

--- back-Landry-Portfolio/app.js
+++ back-Landry-Portfolio/app.js
@@ -1,16 +1,36 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const mailRouter = require('./controllers/mailWs.js');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-const mailRouter = require('./controllers/mailWs.js'); 
+const corsOptions = {
+  origin: ['http://localhost:3000'],
+  methods: ['POST'],
+  allowedHeaders: ['Content-Type'],
+};
+app.use(cors(corsOptions));
 
-app.use(cors());
+app.use(helmet());
+
+const emailLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, 
+  max: 10, 
+  message: "Trop de requêtes, réessayez plus tard.",
+});
+app.use('/email', emailLimiter);
+
+
 app.use(express.json());
+
 
 app.use('/', mailRouter);
 
+
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+  console.log(`Serveur sécurisé lancé sur le port ${port}`);
 });
